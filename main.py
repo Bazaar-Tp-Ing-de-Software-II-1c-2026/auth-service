fa4b091031a8bd9cb22ab7c2641f12aaa1a36e2d
cvaldatta <cvaldatta@fi.uba.ar>
2026-04-01 03:21:26 -0300

    feat(auth): implemented auth microservice and first try connectng to frontend repository

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 from app.database import engine
 from app import models
 from app.api import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Bazaar Auth Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 
