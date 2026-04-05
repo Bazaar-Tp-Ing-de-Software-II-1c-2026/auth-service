098d680199c7445e5284240ba2ae932ac577cc40
cvaldatta <cvaldatta@fi.uba.ar>
2026-04-05 01:27:51 -0300

    fix(): added models y schemas

--- app/schemas.py
+++ app/schemas.py
@@ -63,6 +63,10 @@ class UserLogin(BaseModel):
 class UserUpdate(BaseModel):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
+    description: Optional[str] = None 
+    
+    class Config:
+        from_attributes = True
 
 
 class UserOut(BaseModel):
